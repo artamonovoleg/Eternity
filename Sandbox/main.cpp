95e9e726bfee2b0525ba28fcde7a2cae97ce353e
artamonovoleg <artamonovav2003@gmail.com>
2020-12-24 15:50:40 +0300

    1. Create instance. Setup debug

--- Sandbox/main.cpp
+++ Sandbox/main.cpp
@@ -1,34 +1,38 @@
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 
-#include <iostream>
-#include <fstream>
-#include <stdexcept>
 #include <algorithm>
 #include <vector>
-#include <cstring>
-#include <cstdlib>
-#include <cstdint>
 
 #include "Base.hpp"
 #include "VulkanHelper.hpp"
-
-
-
 
 namespace Eternity
 {
     class VulkanRenderer
     {
         private:
+            struct QueueFamilyIndices
+            {
+                std::optional<uint32_t> graphicsFamily;
+                bool IsComplete()
+                {
+                    return graphicsFamily.has_value();
+                }
+            };
+
             VkInstance          m_Instance          = VK_NULL_HANDLE;
 
             VkPhysicalDevice    m_PhysicalDevice    = VK_NULL_HANDLE;
+            VkDevice            m_LogicalDevice     = VK_NULL_HANDLE;
 
             void CreateInstance();
             void DestroyInstance();
 
+            QueueFamilyIndices FindQueueFamilies(const VkPhysicalDevice& device);
+            bool IsDeviceSuitable(const VkPhysicalDevice& device);
             void PickPhysicalDevice();
+
             void CreateLogicalDevice();
             void DestroyLogicalDevice();
         public:
@@ -41,10 +45,12 @@ namespace Eternity
         CreateInstance();
         VulkanHelper::CreateDebugMessenger(m_Instance);
         PickPhysicalDevice();
+        CreateLogicalDevice();
     }
 
     VulkanRenderer::~VulkanRenderer()
     {
+        DestroyLogicalDevice();
         VulkanHelper::DestroyDebugMessenger(m_Instance);
         DestroyInstance();
     }
@@ -82,13 +88,63 @@ namespace Eternity
         vkDestroyInstance(m_Instance, nullptr);
     }
 
+    VulkanRenderer::QueueFamilyIndices VulkanRenderer::FindQueueFamilies(const VkPhysicalDevice& device)
+    {
+        QueueFamilyIndices indices;
+
+        uint32_t queueFamilyCount = 0;
+        // get queue family count
+        vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, nullptr);
+        std::vector<VkQueueFamilyProperties> queueFamilies(queueFamilyCount);
+        // get queue family properties
+        vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, queueFamilies.data());
+
+        // now find queue family which we need
+        int i = 0;
+        for (const auto& queueFamily : queueFamilies)
+        {
+            if (queueFamily.queueFlags & VK_QUEUE_GRAPHICS_BIT)
+                indices.graphicsFamily = i;
+
+            if (indices.IsComplete())
+                break;
+
+            i++;
+        }
+
+        return indices;
+    }
+
+    bool VulkanRenderer::IsDeviceSuitable(const VkPhysicalDevice& device)
+    {
+        QueueFamilyIndices indices = FindQueueFamilies(device);
+        return indices.IsComplete();
+    }
+
     void VulkanRenderer::PickPhysicalDevice()
     {
-
+        uint32_t deviceCount = 0;
+        // get gpus count last parameter nullptr
+        vkEnumeratePhysicalDevices(m_Instance, &deviceCount, nullptr);
+        ET_CORE_ASSERT(deviceCount != 0, "No gpu");
+        std::vector<VkPhysicalDevice> devices(deviceCount);
+        // now enumerate again and save all gpus to vector
+        vkEnumeratePhysicalDevices(m_Instance, &deviceCount, devices.data());
+        // Find suitable gpu from list of all founded
+        for (const auto& device : devices)
+        {
+            if (IsDeviceSuitable(device))
+            {
+                m_PhysicalDevice = device;
+                break;
+            }
+        }
+        ET_CORE_ASSERT(m_PhysicalDevice != VK_NULL_HANDLE, "No suitable gpu");
     }
 
     void VulkanRenderer::CreateLogicalDevice()
     {
+
     }
 
     void VulkanRenderer::DestroyLogicalDevice()
