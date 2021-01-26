3b003f676e30fca6c5c7610b76905927be7ff16a
artamonovoleg <artamonovav2003@gmail.com>
2021-01-26 10:06:21 +0300

    clean

--- Engine/Core/Log.hpp
+++
@@ -1,6 +0,0 @@
-#include "Logger.hpp"
-
-#define ET_CORE_INFO(...)   ::Eternity::Logger::Info(__VA_ARGS__)
-#define ET_CORE_TRACE(...)  ::Eternity::Logger::Trace(__VA_ARGS__)
-#define ET_CORE_WARN(...)   ::Eternity::Logger::Warn(__VA_ARGS__)
-#define ET_CORE_ERROR(...)  ::Eternity::Logger::Error(__VA_ARGS__)
--- Engine/main.cpp
+++ Engine/main.cpp
@@ -1,30 +1,7 @@
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
-#include "Window.hpp"
-#include "VulkanRenderer.hpp"
-#include "EventSystem.hpp"
-#include "Input.hpp"
-#include "VulkanHelper.hpp"
-#include <vulkan/vulkan.hpp>
 
 int main(int argc, char** argv) 
 {
-    Eternity::CreateWindow(800, 600, "Eternity");
-    Eternity::EventSystem::Init();
-    Eternity::Input::Init();
-
-    Eternity::VulkanRenderer renderer;
-    
-    renderer.SetTargetModel("../Engine/assets/diablo.obj", "../Engine/assets/diablo.tga");
-    renderer.InitVulkan();
-    
-    while (!glfwWindowShouldClose(Eternity::GetCurrentWindow()))
-    {
-        renderer.DrawFrame();
-        Eternity::EventSystem::PollEvents();
-    }
-
-    renderer.DeinitVulkan();
-    Eternity::DestroyWindow();
     return 0; 
 }
