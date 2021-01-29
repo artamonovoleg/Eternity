6961efd45c66407b972be7bdfa4074c25c458890
artamonovoleg <artamonovav2003@gmail.com>
2021-01-29 18:27:26 +0300

    work. start scene implementing

--- Engine/main.cpp
+++ Engine/main.cpp
@@ -1,33 +1,3 @@
-// #define GLFW_INCLUDE_VULKAN
-// #include <GLFW/glfw3.h>
-
-// #include "Base.hpp"
-// #include "Window.hpp"
-// #include "Instance.hpp"
-
-// int main(int, char**) 
-// {
-//     ET_INFO("Info example");
-//     ET_TRACE("Trace example");
-//     ET_WARN("Warning example");
-//     ET_ERROR("Error example");
-
-//     Eternity::CreateWindow(800, 600, "H");
-
-//     std::shared_ptr<Eternity::Instance> instance = std::make_shared<Eternity::Instance>();
-
-//     // Eternity::Instance instance;
-
-//     while (!Eternity::WindowShouldClose())
-//     {
-//         Eternity::PollEvents();
-//     }
-    
-//     Eternity::DestroyWindow();
-
-//     return 0;
-// }
-
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 
