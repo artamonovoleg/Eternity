2b5b3881ff950a48d208d5fdb99cf601772c34d6
artamonovoleg <artamonovav2003@gmail.com>
2021-02-19 20:24:04 +0300

    Chunk

--- Engine/Sandbox/Chunk.cpp
+++ Engine/Sandbox/Chunk.cpp
@@ -1,43 +1,5 @@
 #include "Chunk.hpp"
 #include "Block.hpp"
-///
-static  std::vector<Vertex> block { 
-            { .pos = glm::vec3(-0.5, -0.5, 0.5), .texCoord = glm::vec2(3.0f / 16.0f, 1.0f / 16.0f) },
-            { .pos = glm::vec3(0.5, -0.5, 0.5), .texCoord = glm::vec2(4.0f / 16.0f, 1.0f / 16.0f) },
-            { .pos = glm::vec3(-0.5, 0.5, 0.5), .texCoord = glm::vec2(3.0f / 16.0f, 0.0f) },
-            { .pos = glm::vec3(0.5, 0.5, 0.5), .texCoord = glm::vec2(4.0f / 16.0f, 0.0f) },
-    // right
-            { .pos = glm::vec3(0.5, -0.5, 0.5), .texCoord = glm::vec2(3.0f / 16.0f, 1.0f / 16.0f) },
-            { .pos = glm::vec3(0.5, -0.5, -0.5), .texCoord = glm::vec2(4.0f / 16.0f, 1.0f / 16.0f) },
-            { .pos = glm::vec3(0.5, 0.5, 0.5), .texCoord = glm::vec2(3.0f / 16.0f, 0.0f) },
-            { .pos = glm::vec3(0.5, 0.5, -0.5), .texCoord = glm::vec2(4.0f / 16.0f, 0.0f) },
-
-    // back
-            { .pos = glm::vec3(0.5, -0.5, -0.5), .texCoord = glm::vec2(3.0f / 16.0f, 1.0f / 16.0f) },
-            { .pos = glm::vec3(-0.5, -0.5, -0.5), .texCoord = glm::vec2(4.0f / 16.0f, 1.0f / 16.0f) },
-            { .pos = glm::vec3(0.5, 0.5, -0.5), .texCoord = glm::vec2(3.0f / 16.0f, 0.0f) },
-            { .pos = glm::vec3(-0.5, 0.5, -0.5), .texCoord = glm::vec2(4.0f / 16.0f, 0.0f) },
-
-    // left
-            { .pos = glm::vec3(-0.5, -0.5, -0.5), .texCoord = glm::vec2(3.0f / 16.0f, 1.0f / 16.0f) },
-            { .pos = glm::vec3(-0.5, -0.5, 0.5), .texCoord = glm::vec2(4.0f / 16.0f, 1.0f / 16.0f) },
-            { .pos = glm::vec3(-0.5, 0.5, -0.5), .texCoord = glm::vec2(3.0f / 16.0f, 0.0f) },
-            { .pos = glm::vec3(-0.5, 0.5, 0.5), .texCoord = glm::vec2(4.0f / 16.0f, 0.0f) },
-
-    // up
-            { .pos = glm::vec3(-0.5, 0.5, -0.5), .texCoord = glm::vec2(0, 0) },
-            { .pos = glm::vec3(-0.5, 0.5, 0.5), .texCoord = glm::vec2(0, 1.0f / 16.0f) },
-            { .pos = glm::vec3(0.5, 0.5, -0.5), .texCoord = glm::vec2(1.0f / 16.0f, 0) },
-            { .pos = glm::vec3(0.5, 0.5, 0.5), .texCoord = glm::vec2(1.0f / 16.0f, 1.0f / 16.0f) },
-
-    // down
-            { .pos = glm::vec3(0.5, -0.5, -0.5), .texCoord = glm::vec2(2.0f / 16.0f, 1.0f / 16.0f) },
-            { .pos = glm::vec3(0.5, -0.5, 0.5), .texCoord = glm::vec2(3.0f / 16.0f, 1.0f / 16.0f) },
-            { .pos = glm::vec3(-0.5, -0.5, -0.5), .texCoord = glm::vec2(2.0f / 16.0f, 0.0f) },
-            { .pos = glm::vec3(-0.5, -0.5, 0.5), .texCoord = glm::vec2(3.0f / 16.0f, 0.0f) } };
-
-static std::vector<uint32_t> inds { 0, 1, 2, 2, 1, 3, 4, 5, 6, 6, 5, 7, 8, 9, 10, 10, 9, 11, 12, 13, 14, 14, 13, 15, 16, 17, 18, 18, 17, 19, 20, 21, 22, 22, 21, 23 };
-///
 
 Chunk::Chunk(glm::ivec3 pos)
     : m_Vertices(vertices), m_Indices(indices), m_Pos(pos)
